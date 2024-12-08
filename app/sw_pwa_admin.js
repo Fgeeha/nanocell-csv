f292bac9168515fbdb4f0ed22fc3c5f2e76f2fc9
cedric bonjour <cbonjour37@gmail.com>
2024-12-08 20:41:49 +0100

    version 10 ready for open source release

--- app/sw_pwa_admin.js
+++ app/sw_pwa_admin.js
@@ -1,22 +1,22 @@
-const versionName = 'Beta_v0.0.9';
+const versionName = 'Beta_v0.0.10';
 const filesToCache = [
-  //  auto input
+//  auto input
 ];
 
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(versionName).then(cache => { return cache.addAll(filesToCache) }))
-  // log_to_db("install");
+e.waitUntil(caches.open(versionName).then(cache => { return cache.addAll(filesToCache) }))
+// log_to_db("install");
 });
 
 self.addEventListener('activate', e => {
-  console.log('Service worker activating...');
-  // e.waitUntil(self.registration?.navigationPreload.enable());
-  // e.waitUntil(clients.claim());
-  e.waitUntil(caches.open(versionName).then(cache => { return cache.addAll(filesToCache) }))
+console.log('Service worker activating...');
+// e.waitUntil(self.registration?.navigationPreload.enable());
+// e.waitUntil(clients.claim());
+e.waitUntil(caches.open(versionName).then(cache => { return cache.addAll(filesToCache) }))
 
-  e.waitUntil(deleteOldCaches());
-  console.log('Service worker activation done');
+e.waitUntil(deleteOldCaches());
+console.log('Service worker activation done');
 });
 
 
@@ -25,21 +25,20 @@ self.skipWaiting();
 
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => { return response || fetch(event.request) })
-      .catch(error => { console.error("Couldn't fetch: " + event.request.url, error) })
-  );
+event.respondWith(
+caches.match(event.request)
+.then(response => { return response || fetch(event.request) })
+.catch(error => { console.error("Couldn't fetch: " + event.request.url, error) })
+);
 });
 
 const deleteCache = async (key) => {
-  await caches.delete(key);
+await caches.delete(key);
 };
 
 const deleteOldCaches = async () => {
-  const cacheKeepList = [versionName];
-  const keyList = await caches.keys();
-  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-  await Promise.all(cachesToDelete.map(deleteCache));
+const cacheKeepList = [versionName];
+const keyList = await caches.keys();
+const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+await Promise.all(cachesToDelete.map(deleteCache));
 };
-
